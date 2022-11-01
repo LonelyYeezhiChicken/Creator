57e9189ab5c1a7eebe6083e4ff235be3ee6821f9
chicken <hibary.taiwan@gmail.com>
2022-11-01 23:49:03 +0800

    feat: model

---
+++ src/Creator.Model/Enum/CodeTitle.cs
@@ -0,0 +1,19 @@
+﻿namespace Creator.Model.Enum
+{
+    /// <summary>
+    /// Code 生成區塊
+    /// </summary>
+    public enum CodeTitle
+    {
+        Service = 1,
+        Repostiory = 2,
+        Controller = 3,
+        ApiController = 4,
+        Dto = 5,
+        Dao = 6,
+        Model = 7,
+        ViewModel = 8,
+        IService = 9,
+        IRepostiory = 10,
+    }
+}
