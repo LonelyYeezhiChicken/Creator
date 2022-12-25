84656952249669629eab1656b98ac82fe4ade670
chicken <hibary.taiwan@gmail.com>
2022-12-25 13:31:56 +0800

    feat: csharp service template

--- src/Creator.Model/Template/CSharp/CsService.cs
+++ src/Creator.Model/Template/CSharp/CsService.cs
@@ -8,7 +8,67 @@ namespace Creator.Model.Template.CSharp
 {
     public class CsService
     {
+
+        public string NameSpace { get; private set; }
+        public string Name { get; private set; }
+
+
+        public CsService(string nameSpace, string name)
+        {
+            NameSpace = string.IsNullOrEmpty(nameSpace) ? throw new AggregateException(nameof(nameSpace)) : nameSpace;
+            Name = string.IsNullOrEmpty(name) ? throw new AggregateException(nameof(name)) : name;
+        }
+
+
+        /// <summary>
+        /// interface 區塊
+        /// </summary>
+        private string InterFaceBlock
+        {
+            get
+            {
+                return $@" internal interface I{Name}
+    {{
        
+    }}";
+            }
+        }
+
+        /// <summary>
+        /// 實作區塊
+        /// </summary>
+        private string ImpBlock
+        {
+            get
+            {
+                return $@"
+internal class {Name}Service : I{Name}
+    {{
+       
+    }}";
+            }
+        }
+
+        /// <summary>
+        /// NameSpace 區塊
+        /// </summary>
+        private string NameSpaceBlocek
+        {
+            get
+            {
+                return $@"namespace {NameSpace}
+{{
+{InterFaceBlock}
+
+{ImpBlock}
+}}";
+            }
+        }
+
+        public string Get()
+        {
+            return NameSpaceBlocek;
+        }
 
     }
 }
---
+++ src/Creator.Test/ModelTest/CsServiceModelTest.cs
@@ -0,0 +1,34 @@
+﻿using Creator.Model.Template.CSharp;
+using NUnit.Framework;
+
+namespace Creator.Test.ModelTest
+{
+    [TestFixture]
+    internal class CsServiceModelTest
+    {
+        [Test]
+        public void GetTest()
+        {
+            CsService csService = new(@"Creator.Model.Template.CSharp", "Test");
+            string actual = csService.Get();
+
+            string expected = @"namespace Creator.Model.Template.CSharp
+{
+ internal interface ITest
+    {
+       
+    }
+
+
+internal class TestService : ITest
+    {
+       
+    }
+}";
+            Console.WriteLine(actual);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+    }
+}
