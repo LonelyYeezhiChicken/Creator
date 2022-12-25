fdc4d0b0961b7a113b363152544f2bd581e5c59c
chicken <hibary.taiwan@gmail.com>
2022-12-25 15:17:00 +0800

    feat: Creator.App

---
+++ src/Creator.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Creator.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/Creator.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Creator.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
